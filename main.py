9d81f651468fd30bf58c524a5c8ce2f67802a236
rekha75s <rekhapr@gmail.com>
2022-06-17 21:01:14 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+from flask import Flask
+app=Flask(__name__)
+@app.route('/')
+def index():
+    return '<h1> Hello world</h1>'
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello,{}!</h1>'.format(name)
+if __name__=='__main__':
+    app.run(debug=True)
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
