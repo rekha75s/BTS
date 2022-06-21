c3653036e338538d61141aacaa141e7389ba481c
rekha75s <rekhapr@gmail.com>
2022-06-21 12:12:01 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from flask import Flask
 app=Flask(__name__)
 @app.route('/')
-def index():
+@app.route('/home')
+def home():
     return '<h1> Hello world</h1>'
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello,{}!</h1>'.format(name)
+
 if __name__=='__main__':
     app.run(debug=True)
 
