df014b8440cbb76090389baf1d5b90370ff07972
rekha75s <rekhapr@gmail.com>
2022-06-24 16:28:11 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template,redirect,url_for,request
 import os
+import pandas as pd
 app=Flask(__name__)
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template('NewInvoice.html')
+@app.route('/success',methods=['POST','GET'])
+def success():
+    if request.method=='POST':
+        ino=request.form['ino']
+        company=request.form['company']
+        dict1 = {'no': [ino], 'company': [company]}
+        df = pd.read_csv(r"bills.csv", index_col=0)
+        ind = df.tail(1).index
+        df.loc[ind[0] + 1] = [ino, company]
+        print(df)
+        df.to_csv('bills.csv')
+        return 'Success'
+
 if __name__=='__main__':
     app.run(debug=True)
 
